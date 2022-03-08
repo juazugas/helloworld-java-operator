79c747887ef8f62badb35f2e4229e85fd7fcad7f
Juan Zuriaga <jzuriaga@redhat.com>
2022-03-08 18:07:52 +0100

    Use the interface method for cleanUp the deletion

--- src/main/java/com/example/HelloWorldAppReconciler.java
+++ src/main/java/com/example/HelloWorldAppReconciler.java
@@ -6,15 +6,13 @@ import org.slf4j.LoggerFactory;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.ReconcilerUtils;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
 public class HelloWorldAppReconciler implements Reconciler<HelloWorldApp> {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloWorldAppReconciler.class);
-
-    // something like "helloworldapps.apps.example.com/finalizer";
-    private static final String HELLOWORLDAPP_FINALIZER = ReconcilerUtils.getDefaultFinalizerName(HelloWorldApp.class);
 
     private final KubernetesClient client;
 
@@ -24,32 +22,20 @@ public class HelloWorldAppReconciler implements Reconciler<HelloWorldApp> {
 
     @Override
     public UpdateControl<HelloWorldApp> reconcile(HelloWorldApp resource, Context context) {
-
         LOG.info("reconciling ..."+resource);
-
-        // Check if the CR is marked to be deleted
-        boolean isInstanceToBeDeleted = resource.getMetadata().getDeletionTimestamp() != null;
-        if (isInstanceToBeDeleted) {
-            LOG.info("Instance marked for deletion, running finalizers");
-            if (resource.getFinalizers().contains(HELLOWORLDAPP_FINALIZER)) {
-                finalizeHelloWorldApp(resource);
-                LOG.info("finalizers completed for instance");
-                resource.removeFinalizer(HELLOWORLDAPP_FINALIZER);
-                return UpdateControl.updateResource(resource);
-            }
-            LOG.info("Instance can be deleted now");
-            return UpdateControl.noUpdate();
-        }
 
         return UpdateControl.noUpdate();
     }
 
-    private void finalizeHelloWorldApp(HelloWorldApp resource) {
+    @Override
+    public DeleteControl cleanup(HelloWorldApp resource, Context context) {
         // TODO(user): Add the cleanup steps that the operator
         // needs to do before the CR can be deleted. Examples
         // of finalizers include performing backups and deleting
         // resources that are not owned by this CR, like a PVC.
         LOG.info("Successfully finalized HelloWorldApp");
+        return DeleteControl.defaultDelete();
     }
+
 }
 
